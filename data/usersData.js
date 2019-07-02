9a9d3603914efd72487c127f7a5edcad8ea1c637
Akili1999 <sluground@gmail.com>
2019-07-02 06:14:10 -0400

    survey doesn't work, rest of development is done

---
+++ data/usersData.js
@@ -0,0 +1,28 @@
+var usersData = [
+  {
+    userName: "D.J",
+    link:
+      "https://media.istockphoto.com/photos/cool-african-man-picture-id544358094?k=6&m=544358094&s=612x612&w=0&h=Z4tSUuw-aZyxXqwvBgi9j0O3QZzQzcpJVCm6nulS1kw=",
+    number: [5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    userName: "Aubree",
+    link:
+      "https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    number: [3, 4, 5, 1, 2, 3, 4, 5, 1]
+  },
+  {
+    userName: "Stephen",
+    link:
+      "https://media.istockphoto.com/photos/happy-laughing-man-picture-id544358212?k=6&m=544358212&s=612x612&w=0&h=odURMNz2hty8LIfpVahaaUKpGU4vd-UlZx4jy-OAnJA=",
+    number: [1, 3, 4, 2, 5, 1, 3, 2, 3]
+  },
+  {
+    userName: "Elise",
+    link:
+      "https://us.123rf.com/450wm/wavebreakmediamicro/wavebreakmediamicro1507/wavebreakmediamicro150739545/42518634-happy-woman-smiling-at-the-beach.jpg?ver=6",
+    number: [2, 3, 1, 4, 3, 2, 5, 3, 1]
+  }
+];
+
+module.exports = usersData;
